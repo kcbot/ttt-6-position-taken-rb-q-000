15c7727957f22ed09ca6a2e9d463dc00aa0be071
kcbot <kc.enriquez@gmail.com>
2016-03-06 22:21:53 +0800

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+
+#board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def position_taken?(board,position)
+           
+    !((board[position] == " ") || (board[position] == "") || (board[position] == nil))
+    
+end
